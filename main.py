3db1f890d4fd7a2820ccf7979c0d0d20eab3e524
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-28 14:18:34 +0300

    Задача №1

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from customtkinter import *
+import ctkmessagebox3 as messages
 
-def button_event():
-    print()
+import modul.modul
+
+
+def button_click():
+    fullname = entry_fullname.get()
+    mail = entry_mail.get()
+    phone = entry_phone.get()
+    login = entry_login.get()
+    password = entry_password.get()
+
+    if fullname == '':
+        messages.showwarning(master=main_wind,title = 'Ошибка ввода',message=f'Поле "Полное имя" не может быть пустым')
+        return
+    elif mail == '':
+        messages.showwarning(master=main_wind,title = 'Ошибка ввода',message=f'Поле "mail" не может быть пустым')
+        return
+    elif phone == '':
+        messages.showwarning(master=main_wind,title = 'Ошибка ввода',message=f'Поле "phone" не может быть пустым')
+        return
+    elif login == '':
+        messages.showwarning(master=main_wind,title = 'Ошибка ввода',message=f'Поле "login" не может быть пустым')
+        return
+    elif password == '':
+        messages.showwarning(master=main_wind,title = 'Ошибка ввода',message=f'Поле "password" не может быть пустым')
+        return
+
+    user_data = modul.modul.FormaSubmit(fullname,mail,phone,login,password)
+    entry_fullname.delete(0,END)
+    entry_mail.delete(0, END)
+    entry_phone.delete(0, END)
+    entry_login.delete(0, END)
+    entry_password.delete(0, END)
+    print(user_data)
+    messages.showsuccess(master=main_wind, title='Информация', message='Сообщение успешно отправлено')
+
+
+
 
 main_wind = CTk()
 main_wind.title('Форма ввода данных')
@@ -10,10 +46,10 @@ main_wind.resizable(False, False)
 main_wind.configure(fg_color=('#2b2f40', 'gray14'))
 
 
-label = CTkLabel(main_wind, text="Полное имя", fg_color="transparent", text_color=("#ffff00", "#DCE4EE"))
-label.pack()
-entry = CTkEntry(main_wind, width=500, placeholder_text="Иванов Иван Иванович")
-entry.pack()
+label_fullname = CTkLabel(main_wind, text="Полное имя", fg_color="transparent", text_color=("#ffff00", "#DCE4EE"))
+label_fullname.pack()
+entry_fullname = CTkEntry(main_wind, width=500, placeholder_text="Иванов Иван Иванович")
+entry_fullname.pack()
 
 frame_1 = CTkFrame(main_wind, width = 900, height=500)
 frame_1.configure(fg_color=('#3f5156', 'gray14'))
@@ -43,7 +79,7 @@ label_password.grid(row=2, column=1, padx=20, pady=20)
 entry_password = CTkEntry(frame_1, width=230, placeholder_text="password")
 entry_password.grid(row=3, column=1, padx=20, pady=20)
 
-butt_submit = CTkButton(main_wind, text="Submit", command=button_event, hover = True)
+butt_submit = CTkButton(main_wind, text="Submit", command=button_click, hover = True)
 butt_submit.pack()
 
 
--- modul/modul.py
+++ modul/modul.py
@@ -9,19 +9,6 @@ class FormaSubmit:
         self.__password = password
 
     def __str__(self):
-        return (f'Полное имя: {self.get_name()}\n'
+        return (f'Полное имя: {self.__fullname}\n'
                 f'почта: {self.__e_mail}, телефон: {self.__phone}\n'
                 f'логин: {self.__login}\nпароль: {self.__password}')
-
-
-    def get_name(self):
-        lst = self.__fullname.split(' ')
-        return lst[0], lst[1], lst[2]
-
-    def set_fullname(self, name):
-        self.__fullname = name
-
-
-
-
-
